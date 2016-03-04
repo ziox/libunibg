b70076a3bd4ac45a3c79ade57710d58919012ce2
Nicola Pasta <nicola.pasta@gmail.com>
2016-03-04 16:58:59 +0100

    Setup

---
+++ src/main/java/it/unibg/Main.java
@@ -0,0 +1,9 @@
+package it.unibg;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/it/unibg/MainTest.java
@@ -0,0 +1,14 @@
+package it.unibg;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest
+{
+    @Test
+    public void test()
+    {
+        assertEquals(2 + 2, 4);
+    }
+}
